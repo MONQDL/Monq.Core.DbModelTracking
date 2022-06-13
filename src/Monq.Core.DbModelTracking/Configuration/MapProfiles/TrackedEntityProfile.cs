a5b8f908345d764aabea75296f579cec9ea00832
2nnar <lobovyroslav@gmail.com>
2022-06-14 00:07:13 +0300

    🔖 3.0.3, messages were translated to English

--- src/Monq.Core.DbModelTracking/Configuration/MapProfiles/TrackedEntityProfile.cs
+++ src/Monq.Core.DbModelTracking/Configuration/MapProfiles/TrackedEntityProfile.cs
@@ -11,20 +11,20 @@ namespace Monq.Core.DbModelTracking.Configuration.MapProfiles
                 .ForMember(x => x.CreatedAt, m => m.MapFrom(p => p.CreatedAt.ToUnixTimeSeconds()))
                 .ForMember(x => x.CreatedBy, m => m.MapFrom(p => p.CreatedBy))
                 .ForMember(x => x.CreatedByName,
-                    m => m.MapFrom(p => p.CreatedBy != null ? p.CreatedByName : $"{p.CreatedByName} (удален)"))
+                    m => m.MapFrom(p => p.CreatedBy != null ? p.CreatedByName : $"{p.CreatedByName} (deleted)"))
                 .ForMember(x => x.UpdatedAt,
                     m => m.MapFrom(p => p.UpdatedAt != null ? (long?)p.UpdatedAt.Value.ToUnixTimeSeconds() : null))
                 .ForMember(x => x.UpdatedBy, m => m.MapFrom(p => p.UpdatedBy))
                 .ForMember(x => x.UpdatedByName,
-                    m => m.MapFrom(p => p.UpdatedBy != null ? p.UpdatedByName : $"{p.UpdatedByName} (удален)"));
+                    m => m.MapFrom(p => p.UpdatedBy != null ? p.UpdatedByName : $"{p.UpdatedByName} (deleted)"));
 
             CreateMap<TrackedEntity, Models.v2.TrackedEntityViewModel>()
                 .ForMember(x => x.CreatedAt, m => m.MapFrom(p => p.CreatedAt))
                 .ForMember(x => x.CreatedBy, m => m.MapFrom(p => p.CreatedBy))
-                .ForMember(x => x.CreatedByName, m => m.MapFrom(p => p.CreatedBy != null ? p.CreatedByName : $"{p.CreatedByName} (удален)"))
+                .ForMember(x => x.CreatedByName, m => m.MapFrom(p => p.CreatedBy != null ? p.CreatedByName : $"{p.CreatedByName} (deleted)"))
                 .ForMember(x => x.UpdatedAt, m => m.MapFrom(p => p.UpdatedAt))
                 .ForMember(x => x.UpdatedBy, m => m.MapFrom(p => p.UpdatedBy))
-                .ForMember(x => x.UpdatedByName, m => m.MapFrom(p => p.UpdatedBy != null ? p.UpdatedByName : $"{p.UpdatedByName} (удален)"));
+                .ForMember(x => x.UpdatedByName, m => m.MapFrom(p => p.UpdatedBy != null ? p.UpdatedByName : $"{p.UpdatedByName} (deleted)"));
         }
     }
 }
